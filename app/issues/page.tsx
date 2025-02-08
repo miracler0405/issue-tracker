f4735a81c8c48f5a82b40d52b5f3564904541f25
tensai <codetensai45@gmail.com>
2025-02-09 04:09:14 +0900

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import Link from '../components/Link';
+// import Link from 'next/link';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import delay from 'delay';
